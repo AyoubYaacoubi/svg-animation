b74a481c7b7a2f1d9b7ba1188b73df839f8870b6
AyoubYaacoubi <AyoubYaacoubi16@gmail.com>
2021-05-29 15:35:03 +0100

    tweeking

--- app/assets/scripts/modules/_Dot.js
+++ app/assets/scripts/modules/_Dot.js
@@ -11,13 +11,34 @@ class Dot {
     this.numOfDots = 109
     this.cloneDot()
     this.dots = document.querySelectorAll(".dot")
-
+    console.log(this.bgCntr.clientHeight.toFixed(0))
+    // this.calculateNum(
+    //   this.bgCntr.clientHeight.toFixed(0),
+    //   this.bgCntr.clientWidth.toFixed(0)
+    // )
     this.events()
   }
   events() {
     this.dots.forEach((dot, i) => {
-      dot.addEventListener("click", (e) => this.stagger(i))
+      dot.addEventListener("click", () => this.stagger(i))
     })
+  }
+
+  calculateNum(h, w) {
+    let rows
+    let columns
+    if (h % 2 == 0) {
+      rows = ((h - 20) / 20).toFixed(0)
+    } else {
+      rows = ((h - 1 - 20) / 20).toFixed(0)
+    }
+    if (w % 2 == 0) {
+      columns = ((w - 20) / 20).toFixed(0)
+    } else {
+      columns = ((w - 1 - 20) / 20).toFixed(0)
+    }
+    this.rows = rows
+    this.columns = columns
   }
 
   stagger(index) {
@@ -29,6 +50,7 @@ class Dot {
         {value: 1, easing: "easeInOutQuad", duration: 1200},
       ],
       delay: anime.stagger(200, {
+        // grid: [this.rows, this.columns],
         grid: [11, 10],
         from: index,
       }),
